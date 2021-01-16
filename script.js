27854e93dce16f656da47d5a07aeeedb4fc053eb
Bartłomiej Janusz <bartjanu@gmail.com>
2021-01-16 16:58:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// window.addEventListener('scroll', function() {
-//     const parallax = document.querySelector('#para')
+ window.addEventListener('scroll', function() {
+     const parallax = document.querySelector('#para')
 
 //     // console.log(parallax)
-//     let scrollPosition = window.pageYOffset
-
-//     parallax.style.transform = 'translateY(' + scrollPosition * 0.8 + 'px'
+     let scrollPosition = window.pageYOffset
+     parallax.style.transform = 'translateY(' + scrollPosition * 0.8 + 'px'
     
-//   })
+   })
 
 
 
