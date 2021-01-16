5bcc8c73a0ee2b715050e2909cbc7e50bcba3a24
Bartłomiej Janusz <bartjanu@gmail.com>
2021-01-16 15:55:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-window.addEventListener('scroll', function() {
-    const parallax = document.querySelector('#para')
+// window.addEventListener('scroll', function() {
+//     const parallax = document.querySelector('#para')
 
-    // console.log(parallax)
-    let scrollPosition = window.pageYOffset
+//     // console.log(parallax)
+//     let scrollPosition = window.pageYOffset
 
-    parallax.style.transform = 'translateY(' + scrollPosition * 0.8 + 'px'
+//     parallax.style.transform = 'translateY(' + scrollPosition * 0.8 + 'px'
     
-  })
+//   })
 
+
+
+  document.getElementById('myModal').addEventListener('click', closeModal)
+
+  function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+
+  function closeModal(event) {
+    console.log(event.target)
+    if(event.target.id === 'myModal' || event.target.className === 'close') {
+      document.getElementById("myModal").style.display = "none";
+    }
+  }
+  
+  var slideIndex = 1;
+  
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+  
+  //   var captionText = document.getElementById("caption");
+   
+    // When going to the end of gallery
+    if (n > slides.length) {slideIndex = 1}
+    // When going to the beginning of gallery
+    if (n < 1) {slideIndex = slides.length}
+    // Disappear all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+  
+    slides[slideIndex-1].style.display = "block";
+    
+    
+  }
+  
